336f3495d5ae50c09664f2cdbb43e2c7e8b25f38
ahmedamine1990 <ahmedaminechaari@gmail.com>
2020-02-21 02:29:22 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import './styles.scss';
+
+export class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ""
+    };
+
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+      <h3>Our Application Is Alive</h3>
+      <p>This isn’t reality. This — is fantasy.</p>
+      <p>Yes I am quoting Star Trek, I can't help it</p>
+      <p>Yes I am quoting Star Trek, I can't help it</p>
+    </div>
+    );
+  }
+}
+
+
+
+//render(<App />, document.getElementById('app'));
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
