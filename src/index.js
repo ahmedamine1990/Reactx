553342ee22066b72e4ca9034c9ef2be007fde3e2
ahmedamine1990 <ahmedaminechaari@gmail.com>
2020-03-09 17:29:25 +0100

    second
    second axios react

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-import './styles.scss';
+import NextEvents from './nextEvents';
 
 export class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      value: ""
-    };
-
-    //this.handleChange = this.handleChange.bind(this);
-  }
-
   render() {
     return (
-      <div>
-      <h3>Our Application Is Alive</h3>
-      <p>This isn’t reality. This — is fantasy.</p>
-      <p>Yes I am quoting Star Trek, I can't help it</p>
-      <p>Yes I am quoting Star Trek, I can't help it</p>
+      <div class="container">
+        <div class="row">
+          <NextEvents /> 
+        </div>   
     </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"))
